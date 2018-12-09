4459731f837a8e3a32fc0586dcff213c5d60b1cf
Bojidar Tonev <ghost1993ifyy@gmail.com>
2018-12-09 23:05:30 +0200

    Created default asp.net core mvc project

---
+++ Web/TattoShop.Web/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TattoShop.Web
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Web/TattooShop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TattooShop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
