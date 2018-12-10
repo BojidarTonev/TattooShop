b6b12ceeee4cd6a5165a6efe7dd507868ab717c8
Bojidar Tonev <ghost1993ifyy@gmail.com>
2018-12-10 03:08:06 +0200

    Introduced services, added home views and bootstrap template root

---
+++ src/Services/TattooShop.Services/Contracts/IHomeService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using TattooShop.Data.Models;
+
+namespace TattooShop.Services.Contracts
+{
+    public interface IHomeService
+    {
+        IEnumerable<Tattoo> RecentTattoos();
+    }
+}
---
+++ src/Services/TattooShop.Services/HomeService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TattooShop.Data;
+using TattooShop.Data.Models;
+using TattooShop.Services.Contracts;
+
+namespace TattooShop.Services
+{
+    public class HomeService : IHomeService
+    {
+        private readonly TattooShopContext _context;
+        private const int TattoosToTake = 12;
+
+        public HomeService(TattooShopContext context)
+        {
+            this._context = context;
+        }
+
+        public IEnumerable<Tattoo> RecentTattoos()
+        {
+            return this._context.Tattoos.OrderBy(x => x.DoneOn).Take(TattoosToTake).ToList();
+        }
+    }
+}
