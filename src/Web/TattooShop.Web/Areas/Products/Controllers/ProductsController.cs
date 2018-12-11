735a6e0bcfa0cfdd49b8343602a9d7d3851f2330
Bojidar Tonev <ghost1993ifyy@gmail.com>
2018-12-11 20:27:23 +0200

    Introduced products views and fixed a link in the layout footer.

--- src/Web/TattooShop.Web/Areas/Products/Controllers/ProductsController.cs
+++ src/Web/TattooShop.Web/Areas/Products/Controllers/ProductsController.cs
@@ -1,16 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using TattooShop.Services.Contracts;
 
 namespace TattooShop.Web.Areas.Products.Controllers
 {
+    [Area("Products")]
     public class ProductsController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductsService _productsService;
+
+        public ProductsController(IProductsService productsService)
         {
-            return View();
+            this._productsService = productsService;
+        }
+
+        public IActionResult All()
+        {
+            var products = this._productsService.All();
+
+            return View(products);
+        }
+
+        public IActionResult Details(string id)
+        {
+            var product = this._productsService.ProductDetails(id);
+            return this.View(product);
+        }
+
+        public IActionResult AllCategorized(string category)
+        {
+            var products = this._productsService.ProductsByCategory(category);
+
+            return this.View("All", products);
+        }
+
+        [HttpPost]
+        public IActionResult All(string category)
+        {
+            var products = this._productsService.ProductsByCategory(category);
+
+            return this.View("All", products);
         }
     }
 }
