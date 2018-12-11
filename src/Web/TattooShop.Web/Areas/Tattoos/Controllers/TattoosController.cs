33834f36a622b4aa44e97bd3b6d17abea4f8aaaa
Bojidar Tonev <ghost1993ifyy@gmail.com>
2018-12-11 20:41:19 +0200

    Introduced Tattoos to separated controller with views.

---
+++ src/Web/TattooShop.Web/Areas/Tattoos/Controllers/TattoosController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TattooShop.Services.Contracts;
+
+namespace TattooShop.Web.Areas.Tattoos.Controllers
+{
+    [Area("Tattoos")]
+    public class TattoosController : Controller
+    {
+        private readonly ITattoosService _tattoosService;
+
+        public TattoosController(ITattoosService tattoosService)
+        {
+            this._tattoosService = tattoosService;
+        }
+
+        public IActionResult All()
+        {
+            var tattoos = this._tattoosService.All();
+
+            return View(tattoos);
+        }
+
+        public IActionResult Details(string id)
+        {
+            var tattoo = this._tattoosService.Details(id);
+
+            return this.View(tattoo);
+        }
+    }
+}
---
+++ src/Web/TattooShop.Web/Areas/Tattoos/Models/DisplayTattooViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TattooShop.Web.Areas.Tattoos.Models
+{
+    public class DisplayTattooViewModel
+    {
+    }
+}
