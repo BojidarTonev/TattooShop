008030371f7e3e279c020fd62d5cb02fcd541365
Bojidar Tonev <ghost1993ifyy@gmail.com>
2018-12-11 20:31:26 +0200

    Introduced Tattoos service

---
+++ src/Services/TattooShop.Services/Contracts/ITattoosService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TattooShop.Data.Models;
+
+namespace TattooShop.Services.Contracts
+{
+    public interface ITattoosService
+    {
+        IEnumerable<Tattoo> All();
+
+        Tattoo Details(string id);
+    }
+}
---
+++ src/Services/TattooShop.Services/TattoosService.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using TattooShop.Data.Contracts;
+using TattooShop.Data.Models;
+using TattooShop.Services.Contracts;
+
+namespace TattooShop.Services
+{
+    public class TattoosService : ITattoosService
+    {
+        private readonly IRepository<Tattoo> _tattoosRepository;
+
+        public TattoosService(IRepository<Tattoo> tattoosRepository)
+        {
+            this._tattoosRepository = tattoosRepository;
+        }
+
+        public IEnumerable<Tattoo> All() => this._tattoosRepository.All().OrderBy(t => t.DoneOn);
+
+        public Tattoo Details(string id)
+        {
+            var tattoo = this._tattoosRepository.All().Include(t => t.Artist).First(t => t.Id == id);
+
+            return tattoo;
+        }
+    }
+}
